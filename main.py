a575550a67447d75fe6d3d73422f8a1e7fd59afa
stefanoaragoni <69864405+stefanoaragoni@users.noreply.github.com>
2022-08-28 15:53:17 -0600

    BMPs & Photoshoot

--- main.py
+++ main.py
@@ -15,11 +15,30 @@ def glpoint():
     r.glColor(1,1,1) #parametros en rango de 0 a 1
 
     #r.lookAt((100,100,100),(0,0,0),(0,0,0))
+    #t.read('./models/rock.bmp')
+    #r.glLoad('./models/rock.obj', (0,-200,0), (1,1,1),(0,0,0), t)
 
     t.read('./models/apple.bmp')
-    #r.glLoad('./models/rock.obj', (0,-200,0), (1,1,1),(0,0,0), t)
-    r.glLoad('./models/apple.obj', (0,-50,0), (7,7,7),(0,90,0), t)
+
+    #------------------ORIGINAL
+    #r.glLoad('./models/apple.obj', (0,-50,0), (7,7,7),(0,0,0), t)
     
+    #------------------MEDIUM SHOT
+    r.glLoad('./models/apple.obj', (-250,-50,0), (5,5,5),(0,0,0), t)
+    r.glLoad('./models/apple.obj', (250,-50,0), (5,5,5),(0,180,0), t)
+
+    #------------------LOW ANGLE
+    #r.glLoad('./models/apple.obj', (0,-50,0), (7,7,7),(100,-10,0), t)
+
+    #------------------HIGH ANGLE
+    #r.glLoad('./models/apple.obj', (0,-50,0), (7,7,7),(95,0,0), t)
+
+    #------------------DUTCH ANGLE
+    #r.glLoad('./models/apple.obj', (0,-50,0), (7,7,7),(90,0,20), t)
+
+    
+
+
     r.glFinish()
 
 glpoint()
